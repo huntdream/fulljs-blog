bc2d3209e2f7726f947f9158af553a720c43207d
Yu <maoyu960320@hotmail.com>
2018-01-15 15:00:35 +0800

    change repo's name

--- server/routes/poetries.js
+++ server/routes/poetries.js
@@ -5,7 +5,6 @@ const connection = require('../model/connection');
 
 router.get('/', function (req, res) {
     let page = parseInt(req.query.page);
-    console.log(-1 == false);
     try {
         connection.query('select content,title,poets.name from poetries inner join poets on poetries.poet_id=poets.id where name=? limit ?,?', ['李白', (page - 1) * 30, 30], function (err, results, fields) {
             if (err) console.log(err);
