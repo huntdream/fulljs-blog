07d4f03121e48f4e602e2ebadea7ef7b6661f7b1
huntdream <maoyu960320@hotmail.com>
2018-02-18 16:56:20 +0800

    fix column err

--- server/model/convert.js
+++ server/model/convert.js
@@ -6,10 +6,14 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/blog');
 const sql =
-  'select poetries.id,poetries.content,poetries.title,poets.name from poetries  inner join poets where poetries.poet_id=poets.id';
+  'select poetries.id,poetries.content,poetries.title,poets.name as author from poetries  inner join poets where poetries.poet_id=poets.id';
 connection.query(sql, function(err, results, fields) {
   results.map(item => {
     poet = new Poet(item);
-    poet.save(item);
+    poet.save(function(err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
   });
+  console.log('Data immigration successful');
 });
