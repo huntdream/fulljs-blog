6a671011b410df8379bf7ab3495282d049851573
huntdream <maoyu960320@hotmail.com>
2018-04-06 15:20:56 +0800

    set NODE_ENV=production

--- client/src/config/host.js
+++ client/src/config/host.js
@@ -1,2 +1,5 @@
-export const host = 'http://localhost:3000';
-// export const host = 'https://blog.maoyu.info';
+export const host =
+  process.env.NODE_ENV === 'production'
+    ? 'https://blog.maoyu.info'
+    : 'http://localhost:3000'
+// export const host = ;
